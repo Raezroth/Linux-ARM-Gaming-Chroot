56b90eb8e1b37b9ae93b24011efc49da4eb2b2f5
Laar3 <laar@tutanota.com>
2022-06-20 20:57:57 +0930

    Removed useless file.

--- scripts/start_chroot_tmp.sh
+++
@@ -1,7 +0,0 @@
-cd /$CHROOT_PATH/debian_gaming
-sudo mount -o bind /dev dev/
-sudo mount -t devpts devpts dev/pts
-sudo mount -o bind /proc proc/
-sudo mount -o bind /sys sys/
-xhost +local:
-sudo chroot .
