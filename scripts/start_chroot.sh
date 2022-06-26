99ba8b2ee0da4bd50dce9ecbd5a269eeca59202e
Raezroth <47491977+Raezroth@users.noreply.github.com>
2022-06-26 11:45:36 -0700

    added new mounts

--- scripts/start_chroot.sh
+++ scripts/start_chroot.sh
@@ -3,5 +3,7 @@ sudo mount -o bind /dev dev/
 sudo mount -t devpts devpts dev/pts
 sudo mount -o bind /proc proc/
 sudo mount -o bind /sys sys/
-xhost +local:
+sudo mount -o bind /run run/
+sudo mount -o bind /run/user/1000 run/user/1000
+sudo mount -o bind /tmp tmp/
 sudo chroot .
