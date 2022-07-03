afaa6239887e48c3f209bbe7f3133c15c4a9b7c2
Laar3 <laar@tutanota.com>
2022-07-03 13:28:06 +0930

    stuff

--- scripts/Gaming_Chroot_Installer_Posix.sh
+++ scripts/Gaming_Chroot_Installer_Posix.sh
@@ -15,115 +15,77 @@ install_chroot(){
   
   # detect distro and install necessary packages
   # TODO, pmos support (might need some changes to other parts of the script aswell.)
-  [ -d /etc/pacman.d ] && $root pacman -S debootstrap xorg-xhost
-  [ -d /etc/apt/sources.list ] && $root apt install debootstrap xhost-xorg
+  [ -d /etc/pacman.d ] && $root pacman -S debootstrap debian-archive-keyring xorg-xhost xorg-server-xephyr --needed --noconfirm
+  [ -d /etc/apt/sources.list ] && $root apt-get -y install deboostrap debian-archive-keyring x11-xserver-utils
   xhost +local:
   # create chroot
-	$root debootstrap --arch armhf --components=main,universe sid $GAMING_CHROOT https://deb.debian.org/debian
+$root debootstrap --arch armhf --components=main,universe sid $GAMING_CHROOT https://deb.debian.org/debian
   # mount all directories
-	$root mount -t proc /proc $GAMING_CHROOT/proc
-	$root mount -t sysfs /sys $GAMING_CHROOT/sys
-	$root mount --bind /dev $GAMING_CHROOT/dev
-	$root mount -t devpts devpts $GAMING_CHROOT/dev/pts
-	$root mount --bind /run $GAMING_CHROOT/run
-	$root mount --bind /run/user/1000 $GAMING_CHROOT/run/user/1000
-	$root mount -t tmpfs tmpfs $GAMING_CHROOT/tmp
-	$root chmod 1777 $GAMING_CHROOT/proc $GAMING_CHROOT/sys $GAMING_CHROOT/dev \
-			$GAMING_CHROOT/dev/pts $GAMING_CHROOT/run \ 
-		       	$GAMING_CHROOT/run/user/1000 $GAMING_CHROOT/tmp
-	$root chroot $CHROOTDIR /bin/bash << EOF
-		echo "export LC_ALL=\"C\""                 >> /root/.bashrc
-		echo "export LANGUAGE=\"C\""               >> /root/.bashrc
-		echo "export PATH=\$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/games:/usr/local/bin:/usr/local/sbin" >> /root/.bashrc
-		echo "export STEAMOS=1"                    >> /root/.bashrc
-		echo "export STEAM_RUNTIME=1"              >> /root/.bashrc
-		echo "chmod 177 /dev/shm"                  >> /root/.bashrc
-		echo "export MESA_GL_VERSION_OVERRIDE=3.2" >> /root/.bashrc
-		echo "export PAN_MESA_DEBUG=gl3"           >> /root/.bashrc
-		echo "export PULSE_SERVER=127.0.0.1"       >> /root/.bashrc
-		source /root/.bashrc
-		dpkg --add-architecture arm64
-		apt update
-		apt install -y sudo vim make cmake git wget gnupg libx11-dev libgl-dev \
-		libvulkan-dev libtcmalloc-minimal4 libnm0 zenity chromium alsamixergui libsdl2-dev unzip \
-		libgles-dev firefox-esr:arm64 libx11-dev:arm64 libvulkan-dev:arm64 libsdl2-dev:arm64 \
-		libgl-dev:arm64 libc6-dev:arm64 libgles-dev:arm64
-		adduser --home /home/steam-user --gecos "" --disabled-password user
-		echo "user:password" | chpasswd
-		usermod -g users steam-user
-		adduser stema-user sudo
-		su steam-user -
-		cd ~/
-		echo "export SDL_VIDEODRIVER=wayland" >> ~/.bash_profile
-		echo "export WAYLAND_DISPLAY=wayland-1" >> ~/.bash_profile
-		echo "export GDK_BACKEND=wayland" >> ~/.bash_profile
-		echo "export XDG_SESSION_TYPE=wayland" >> ~/.bash_profile
-		mkdir Downloads
-		cd Downloads
-		wget https://github.com/Raezroth/Linux-ARM-Gaming-Chroot/releases/download/Experimental-1/box64_0.1.8_sid_arm64_0606.deb
-		wget https://github.com/Raezroth/Linux-ARM-Gaming-Chroot/releases/download/Experimental-1/box86_0.2.7_sid_nightly_0606.deb
-		wget http://ftp.debian.org/debian/pool/main/libi/libindicator/libindicator7_0.5.0-4_armhf.deb
-		wget http://ftp.debian.org/debian/pool/main/liba/libappindicator/libappindicator1_0.4.92-7_armhf.deb
-		exit
-		apt install -y /home/user/Downloads/*.deb
-		su steam-user -
-		cd ~/Downloads
-		wget https://repo.steampowered.com/steam/archive/stable/steam_latest.deb
-		exit
-		apt install -y /home/user/Downloads/steam_latest.deb
+  cd $GAMING_CHROOT
+  $root mount -t proc /proc proc/
+  $root mount -t sysfs /sys sys/
+  $root mount --bind /dev dev/
+  $root mount -t devpts devpts dev/pts
+  $root mount --bind /run run/
+  $root mount --bind /run/user/1000 run/user/1000
+  $root mount -t tmpfs tmpfs tmp/
+  #TODO there may be a better way to do this but for now its needed to make internet work reliably, maybe a symlink
+  $root cp /etc/resolv.conf etc/resolv.conf
+  $root chroot . <<EOF
+apt-get -y install foot stterm
+
+dpkg --add-architecture arm64 
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install sudo vim make cmake git wget gnupg libx11-dev libgl-dev libvulkan-dev libtcmalloc-minimal4 libnm0 zenity chromium alsamixergui libsdl2-dev unzip libgles-dev firefox-esr:arm64 libx11-dev:arm64 libvulkan-dev:arm64 libsdl2-dev:arm64 libgl-dev:arm64 libc6-dev:arm64 libgles-dev:arm64 xterm
+useradd -m user1
+usermod -aG users sudo user1
+echo "user1:gaming" | chpasswd
+echo "root:gaming" | chpasswd
+chsh user1 -s /bin/bash
+mkdir /home/user1
+chown user1 /home/user1
+echo 'export LC_ALL="C"
+
+export LANGUAGE="C"
+
+export PATH=\$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/games:/usr/local/bin:/usr/local/sbin
+
+export SDL_VIDEODRIVER=wayland
+
+export WAYLAND_DISPLAY=wayland-1
+
+export GDK_BACKEND=wayland
+
+export XDG_SESSION_TYPE=wayland
+
+export XDG_RUNTIME_DIR=/run/user/1000
+
+export DISPLAY=:1
+
+export XSOCKET=/tmp/.X11-unix/X1
+
+export STEAMOS=1
+
+export STEAM_RUNTIME=1' > /home/user1/.bashrc
+source /home/user1/.bashrc
+cd /tmp
+wget 'https://github.com/Raezroth/Linux-ARM-Gaming-Chroot/releases/download/Nightly-0626/box64_0.1.8_arm64_0626.deb'
+wget 'https://github.com/Raezroth/Linux-ARM-Gaming-Chroot/releases/download/Nightly-0626/box86_0.2.6_armhf_0626.deb'
+apt-get -y install ./box*.deb
+
+
 EOF
-	echo "Setting up necessary files."
-	
-	$root cp $GAMING_CHROOT/etc/binfmt.d/box86.conf /etc/binfmt.d
-	$root cp $GAMING_CHROOT/etc/binfmt.d/box64.conf /etc/binfmt.d
-	$root systemctl restart systemd-binfmt
-
-  # Create a helper script to launch the chroot with steam
-	$root echo "cd $GAMING_CHROOT
-       
-sudo mount -t proc /proc $GAMING_CHROOT/proc
-sudo mount -t sysfs /sys $GAMING_CHROOT/sys
-sudo mount --bind /dev $GAMING_CHROOT/dev
-sudo mount -t devpts devpts $GAMING_CHROOT/dev/pts
-sudo mount --bind /run $GAMING_CHROOT/run
-sudo mount --bind /run/user/1000 $GAMING_CHROOT/run/user/1000
-sudo mount -t tmpfs tmpfs $GAMING_CHROOT/tmp
-sudo chmod 1777 $GAMING_CHROOT/proc $GAMING_CHROOT/sys $GAMING_CHROOT/dev $GAMING_CHROOT/dev/pts $GAMING_CHROOT/run $GAM
-ING_CHROOT/run/user/1000 $GAMING_CHROOT/tmp
- sudo chroot $GAMING_CHROOT /bin/bash -x <<'EOF'
-  source /root/.bashrc
-   chmod 1777 /dev/shm
-    su raezroth
-     cd ~/
-      PAN_MESA_DEBUG=gl3 steam -no-cef-sandbox +open steam://open/bigpicture 2> steam.log >> steam.log
-      EOF
-       sudo umount $GAMING_CHROOT/dev/pts $GAMING_CHROOT/run/user/1000 $GAMING_CHROOT/*">> /bin/start-steam.sh
-	
-	$root chmod +x /bin/start-steam.sh
-	$root cp -r $GITDIR/scripts/steam.desktop /usr/share/applications/
-	$root chmod +x /usr/share/applications/steam.desktop
-	# Creates a helper script to get a standard terminal.
-	$root echo "cd $GAMING_CHROOT
-       
-sudo mount -t proc /proc $GAMING_CHROOT/proc
-sudo mount -t sysfs /sys $GAMING_CHROOT/sys
-sudo mount --bind /dev $GAMING_CHROOT/dev
-sudo mount -t devpts devpts $GAMING_CHROOT/dev/pts
-sudo mount --bind /run $GAMING_CHROOT/run
-sudo mount --bind /run/user/1000 $GAMING_CHROOT/run/user/1000
-sudo mount -t tmpfs tmpfs $GAMING_CHROOT/tmp
-sudo chmod 1777 $GAMING_CHROOT/proc $GAMING_CHROOT/sys $GAMING_CHROOT/dev $GAMING_CHROOT/dev/pts $GAMING_CHROOT/run $GAM
-ING_CHROOT/run/user/1000 $GAMING_CHROOT/tmp
- sudo chroot $GAMING_CHROOT 
-      sudo umount $GAMING_CHROOT/dev/pts $GAMING_CHROOT/run/user/1000 $GAMING_CHROOT/*" >> /bin/gaming_chroot_terminal.sh
-
-  $root chmod +x /bin/gaming_chroot_terminal.sh
-
-	echo "Umounting container..."
-
-	$root umount $GAMING_CHROOT/dev/pts $GAMING_CHROOT/run/user/1000 $GAMING_CHROOT/*
-
-	echo "Install Complete! If everything went well you should be able to launch Steam."
+$root cp -r $GAMING_CHROOT/etc/binfmt.d/box* /etc/binfmt.d/
+$root systemctl restart systemd-binfmt
+$root chroot . <<EOF
+cd /tmp
+wget http://ftp.debian.org/debian/pool/main/libi/libindicator/libindicator7_0.5.0-4_armhf.deb
+wget http://ftp.debian.org/debian/pool/main/liba/libappindicator/libappindicator1_0.4.92-7_armhf.deb
+apt-get -y install ./lib*
+wget https://repo.steampowered.com/steam/archive/stable/steam_latest.deb
+apt-get -y install ./steam_latest.deb
+EOF
 
 }
 
